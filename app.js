276775d6ce75251d844859dfc211fd68f2eeea53
ericaleong <59629488+ericaleong@users.noreply.github.com>
2020-03-04 09:39:26 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
 const path = require('path');
 const express = require('express');
+const pageInfo = require('./pageInfo');
 const fs = require('fs');
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+app.get('/', function(request, response){
+  response.render('index', pageInfo.index);
+});
+
+app.get('/about', function(request, response){
+  response.render('about', pageInfo.about);
+});
+
+app.get('/contact', function(request, response){
+  response.render('contact', pageInfo.contact);
+});
+
+app.get('/projects', function(request, response){
+  response.render('projects', pageInfo.projects);
+});
+
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
   res.writeHead(404, {'Content-Type': 'text/html'});
   fs.createReadStream(__dirname + '/404.html').pipe(res)
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, function(){
   console.log(`Listening on port ${PORT}`);
 });
-
