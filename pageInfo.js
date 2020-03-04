113ecee732dcbbd4f1ae3652151d04f6dab4773f
ericaleong <59629488+ericaleong@users.noreply.github.com>
2020-03-04 09:39:28 -0700

    Create pageInfo.js

---
+++ pageInfo.js
@@ -0,0 +1,20 @@
+const pageInfo = {
+  index: {
+    title: 'Home Page',
+    description: 'Pugs are Cute'
+  },
+  about: {
+    title: 'About Page',
+    description: 'Coming soon... A page about pugs and how adorable they are',
+  },
+  projects: {
+    title: 'Projects',
+    description: 'Coming soon... A page on pugs doing projects',
+  },
+  contact: {
+    title: 'Contact Page',
+    description: 'Contact me to learn more about how pugs are thugs!',
+  }
+}
+
+module.exports = pageInfo;
